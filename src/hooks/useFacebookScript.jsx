b3d3e55515ca2c76f36de7d20cf5981d873f122e
Bimal Saggu <bimalsaggu28@gmail..com>
2025-05-08 17:38:35 +0530

    reorder fb script

--- src/hooks/useFacebookScript.jsx
+++ src/hooks/useFacebookScript.jsx
@@ -2,15 +2,6 @@ import { useEffect } from "react";
 
 const useFacebookScript = () => {
   useEffect(() => {
-    // Immediately invoked function expression (IIFE) to load the Facebook SDK
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0]; // Get the first <script> tag
-      if (d.getElementById(id)) return; // If the SDK script is already in the document, don't load it again
-      js = d.createElement(s); js.id = id; // Create a new <script> tag with the given id
-      js.src = "https://connect.facebook.net/en_US/sdk.js"; // Facebook SDK script URL
-      fjs.parentNode.insertBefore(js, fjs); // Insert the script before the first <script> tag
-    }(document, 'script', 'facebook-jssdk')); // Invoke the function immediately
-
     // Initialize the Facebook SDK
     window.fbAsyncInit = function () {
       window.FB.init({
@@ -23,6 +14,15 @@ const useFacebookScript = () => {
       window.FB.AppEvents.logPageView(); // Log page view event
       window.FB.isReady = true; // Mark the SDK as ready
     };
+
+    // Immediately invoked function expression (IIFE) to load the Facebook SDK
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0]; // Get the first <script> tag
+      if (d.getElementById(id)) return; // If the SDK script is already in the document, don't load it again
+      js = d.createElement(s); js.id = id; // Create a new <script> tag with the given id
+      js.src = "https://connect.facebook.net/en_US/sdk.js"; // Facebook SDK script URL
+      fjs.parentNode.insertBefore(js, fjs); // Insert the script before the first <script> tag
+    }(document, 'script', 'facebook-jssdk')); // Invoke the function immediately
   }, []); // Empty dependency array ensures this runs only once, when the component is mounted
 };
 
